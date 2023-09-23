59efd8ce401e34f722c07dc26893fa016c596b91
Dyzio18 <dyzio18@gmail.com>
2023-09-23 19:22:32 +0200

    Update projects & add blog post (next.js)

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,20 +3,16 @@ import Link from './Link';
 
 const Card = ({ title, description, imgSrc, href }) => (
   <div className="md max-w-[544px] p-4 md:w-1/2">
-    <div
-      className={`${
-        imgSrc && 'h-full'
-      }  overflow-hidden rounded-md border-2 border-gray-200 border-opacity-60 dark:border-gray-700`}
-    >
+    <div className="h-full">
       {imgSrc &&
         (href ? (
           <Link href={href} aria-label={`Link to ${title}`}>
             <Image
               alt={title}
               src={imgSrc}
-              className="object-cover object-center md:h-36 lg:h-48"
+              className="object-cover object-center "
               width={544}
-              height={306}
+              height={380}
             />
           </Link>
         ) : (
--- data/projectsData.ts
+++ data/projectsData.ts
@@ -2,15 +2,15 @@ const projectsData = [
   {
     title: 'eqchange - swap platform',
     description: `eqchange is a platform that allows you to exchange items with other users.`,
-    imgSrc: '/static/images/google.png',
+    imgSrc: '/static/images/projects/eqchange-project.png',
     href: 'https://eqchange.pl',
   },
   {
     title: 'selfcodehero - gamification engine & self-development platform',
     description: `selfcodehero is a platform that allows you to learn new skills and develop yourself in a fun way.`,
-    imgSrc: '/static/images/time-machine.jpg',
+    imgSrc: '/static/images/projects/selfcodehero-project.png',
     href: 'https://selfcodehero.com',
   },
-]
+];
 
-export default projectsData
+export default projectsData;
